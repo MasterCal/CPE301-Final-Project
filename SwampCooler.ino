509db7f035a4ca4f3797f733acb31937cd115d7c
mafahar <82742633+mafahar@users.noreply.github.com>
2022-12-02 13:28:45 -0800

    Update SwampCooler.ino

--- SwampCooler.ino
+++ SwampCooler.ino
@@ -1,6 +1,12 @@
+volatile unsigned char *portF  =  (unsigned char *) 0x31;
+volatile unsigned char *portDDRF = (unsigned char *) 0x30; 
+volatile unsigned char *portB  = (unsigned char *) 0x25;
+volatile unsigned char *portDDRB = (unsigned char *) 0x24;
+
 void setup()
 {
-  
+ *portDDRB |= 0x40;
+ *portB &= 0xBF; 
 }
   
 void loop()
